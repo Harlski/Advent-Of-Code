df8e2e759cce5a0b8557ada39d86cf2edfe2e545
Harley <harley.jones1992@gmail.com>
2024-01-19 21:43:46 +1100

    Update

--- 2023/Day-01/rust/day-01/src/main.rs
+++ 2023/Day-01/rust/day-01/src/main.rs
@@ -1,24 +1,28 @@
 use std::env;
 use std::fs;
+use std::process; 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem passing args: {err}");
+        process::exit(1);
+    });
+    
     let contents = fs::read_to_string(config.file_path).expect("Error");
     println!("With text:\n{contents}");
 }
 
 
 impl Config {
-    fn new(args: &[String]) -> Config  {
+    fn build(args: &[String]) -> Result<Config, &'static str>  {
         if args.len() < 3{
-            panic!("Not Enough Args");
+            return Err("Not enough args");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
     
-        Config { query, file_path } 
+        Ok(Config { query, file_path })
     }
 }
 
